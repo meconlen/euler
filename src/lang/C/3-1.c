c3d0488ffe41be60e1004671a7128bc67a37a598
Michael Conlen <mike@conlen.org>
2013-06-04 16:03:56 -0400

    added updates for autoconf

--- src/lang/C/3-1.c
+++ src/lang/C/3-1.c
@@ -5,10 +5,10 @@
 int main(int argc, char *argv[])
 {
 	unsigned long long	n = 600851475143, sn;
-	unsigned long long	i,j,k;
+	unsigned long long	i;
 
 	sn = sqrt(n);
-	while(n & 1 == 0) n = n >> 1;	
+	while((n & 1) == 0) n = n >> 1;	
 	for(i=3; i< sn; i+=2) {
 		while(n%i == 0) { n = n/i; sn = sqrt(n); }
 	}
