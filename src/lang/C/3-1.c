3dc0c7509e6555269f8727b4eeda2988376a8eba
Michael Conlen <mike@conlen.org>
2013-06-06 17:26:14 -0400

    removed old code which was commented out

--- src/lang/C/3-1.c
+++ src/lang/C/3-1.c
@@ -8,13 +8,6 @@ int main(int argc, char *argv[])
 	unsigned long long	n = 600851475143, i, d=0; 
 	unsigned long long	len, *list;	
 
-/*
-	sn = sqrt(n);
-	while((n & 1) == 0) n = n >> 1;	
-	for(i=3; i< sn; i+=2) {
-		while(n%i == 0) { n = n/i; sn = sqrt(n); }
-	}
-*/
 	factorN(n, &list, &len);
 	for(i=0; i<len; i++) {
 		if(d<list[i]) d = list[i];
