d28912e4a2cbd087885bc73c43d5ef9b71afc7ee
Michael Conlen <mike@conlen.org>
2013-06-14 10:06:47 -0400

    wrote twoFactor(). Are these the things we do when no one else will see or are these the things we secretly hope others will find?

--- src/lang/C/factor.c
+++ src/lang/C/factor.c
@@ -64,10 +64,31 @@ error0:
 	return;
 }
 
-// int twoFactor(uint64_t *list, uint64_t len, uint64_t &x, uint64_t &y)
+// int twoFactor(uint64_t *list, uint64_t len, uint64_t *x, uint64_t *y, uint64_t n)
 
 void unit_twoFactor(void)
 {
+	uint64_t	*list = NULL, len = 0, rc;
+	uint64_t	x, y;
+	
+	factorN(2310, &list, &len);
+	if(list == NULL) goto error0;
+	CU_ASSERT((rc = twoFactor(list, len, &x, &y, 0)) == 0);
+	CU_ASSERT(x == 1);
+	CU_ASSERT(y == 2310);
+	CU_ASSERT((rc = twoFactor(list, len, &x, &y, 1)) == 0);
+	CU_ASSERT(x == 2);
+	CU_ASSERT(y == 3*5*7*11);
+	CU_ASSERT((rc = twoFactor(list, len, &x, &y, 2)) == 0);
+	CU_ASSERT(x == 3);
+	CU_ASSERT(y == 2*5*7*11);
+	CU_ASSERT((rc = twoFactor(list, len, &x, &y, 25)) == 0);
+	CU_ASSERT(x == 11*7*2);
+	CU_ASSERT(y == 5*3);
+	CU_ASSERT((rc = twoFactor(list, len, &x, &y, 32)) == -1)
+	free(list);
+	return;
+error0:
 	CU_FAIL("no test");
 	return;
 }
@@ -79,7 +100,7 @@ int factorN(unsigned long long n, unsigned long long **list, unsigned long long 
 	unsigned long long	sn, i, *f;
 	queue			*q;
 
-	q = queueCreate(NULL);
+	if((q = queueCreate(NULL)) == NULL) goto error0;
 	sn = sqrt(n);
 	while((n & 1) == 0) {
 		f = malloc(sizeof(unsigned long long));
@@ -111,11 +132,21 @@ int factorN(unsigned long long n, unsigned long long **list, unsigned long long 
 	}
 	queueDestroy(q);
 	return(0);
+error0:
+	return(-1);
 }
 // end factorN //
 
 
 int twoFactor(uint64_t *list, uint64_t len, uint64_t *x, uint64_t *y, uint64_t n) 
 {
+	uint64_t	i;
+
+	if(n >= (uint64_t)1 << len) goto error0; // n is out of bounds
+	*x = 1; *y = 1;
+	for(i=0; i<len; i++) if((n & ((uint64_t)1 << i)) == 0) *y *= list[i]; else *x *= list[i];
+	return(0);
+
+error0: 
 	return(-1);
 }
