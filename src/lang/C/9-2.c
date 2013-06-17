43aa24a9f1d562c8175b9086e03766a776a74045
Michael Conlen <mike@conlen.org>
2013-06-17 14:34:35 -0400

    finished up problem 9

--- src/lang/C/9-2.c
+++ src/lang/C/9-2.c
@@ -9,12 +9,18 @@
 #include "sieve.h"
 
 // findTriple //
-int findTriple(uint64_t n, uint64_t *a, uint64_t *b, uint64_t *c)
+int findTriple2(uint64_t x, uint64_t *a, uint64_t *b, uint64_t *c)
 {
-	for(*a=1; *a<n; (*a)++) {
-		for(*b=0; *b<*a; (*b)++) {
-			*c=n - (*a) - (*b);
-			if((*a) * (*a) + (*b) * (*b) == (*c) * (*c)) return(0);
+	uint64_t	m, n, k;
+	for(m=1; m<x; m++) {
+		for(n = m%2 == 0 ? 1 : 2; n<m; n+=2) {
+			if(x%(2*m*m + 2*m*n) == 0) {
+				k=x/(2*m*m+2*m*n);
+				*a=k*2*m*n;
+				*b=k*(m*m-n*n);
+				*c=k*(m*m+n*n);
+				return(0);
+			}
 		}
 	}
 	return(-1);
@@ -37,8 +43,22 @@ int main(int argc, char *argv[])
 		}
 	}
 	if(n == 0) goto usage;
-	rc = findTriple(n, &a, &b, &c);	
+	rc = findTriple2(n, &a, &b, &c);	
 	if(rc == 0) printf("a = %llu, b = %llu, c = %llu, abc = %llu\n", a, b, c, a*b*c);
+/*
+	for(a=1; a<n; a++) {
+		for(b=0; b<a; b++) {
+			c=n-a-b;
+			if(a*a+b*b==c*c) {
+				printf("%llu^2 + %llu^2 = %llu^2\n", a*a, b*b, c*c);
+				printf("a = %llu, b = %llu, c = %llu, a + b + c = %llu\n", a, b, c, a+b+c);
+				printf("abc = %llu\n", a*b*c);
+				exit(0);
+			}
+			
+		}
+	}
+*/
 	exit(0);
 error0:
 	exit(-1);
