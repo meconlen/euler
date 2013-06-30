9216b6bc79daba00b40bcbbc9152d5ecbcecdc04
Michael Conlen <mike@conlen.org>
2013-06-30 17:27:38 -0400

    finished up

--- src/lang/C/20-1.c
+++ src/lang/C/20-1.c
@@ -10,24 +10,24 @@
 #include "algebra.h"
 #include "sieve.h"
 
-// findSum //
-int findSum(uint64_t n)
+// factorialDigitSum //
+uint64_t factorialDigitSum(uint64_t n)
 {
 	char		*line = NULL, *cursor;
-	mpz_t		exp;
+	mpz_t		f;
 	uint64_t	x = 0, digits;
 
-	mpz_init(exp);
-	mpz_ui_pow_ui(exp, 2, n);
-	digits = mpz_sizeinbase(exp, 10);
-	line = calloc(digits+2, sizeof(char));
-	mpz_get_str(line, 10, exp);
+	mpz_init(f);
+	mpz_fac_ui(f, n);
+	digits = mpz_sizeinbase(f, 10);
+	line = calloc(digits+2,sizeof(char));
+	mpz_get_str(line, 10, f);
 	cursor = line;
 	while(*cursor != 0) x += (*(cursor++) - '0');
 	free(line);
 	return(x);	
 }
-// end findSum //
+// end factorialDigitSum //
 
 int main(int argc, char *argv[])
 {
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
 	}
 	if(n == 0) goto usage;
 
-	x = findSum(n);
+	x = factorialDigitSum(n);
 	printf("sum = %llu\n", x);
 	exit(0);
 // error0:
